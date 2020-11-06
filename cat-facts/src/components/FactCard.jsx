25376dbdaf3d1013671c97d39f06b38011af5bd4
Shelley Ophir <qidcoder@gmail.com>
2020-11-06 12:35:41 -0500

    Added nested terinary condition on FactCard

--- cat-facts/src/components/FactCard.jsx
+++ cat-facts/src/components/FactCard.jsx
@@ -13,11 +13,15 @@ const FactCard = props => {
                 <h2 className="card-title">{fact.text}</h2>
 
                 {/*The error is that the API has a single instance without a user - so the terinary operator will take care of it by checking for a user*/}
+                {/* adding a nested terinary operator to cover an errors now that we have a list of other animal facts */}
                 <h6 className="card-subtitle text-muted my-3">Posted by: 
                     {fact.user ? 
-                        fact.user.name.first +" "+ fact.user.name.last
-                        :
-                        'Anonymous'
+                        fact.user.name ?
+                            fact.user.name.first +" "+ fact.user.name.last
+                            :
+                            'Anonymous'
+                        : 
+                        "Anonymous"
                     }
                 </h6>
             </div>
